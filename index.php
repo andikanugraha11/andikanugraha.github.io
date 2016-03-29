664e24212e86dcd8e33918c5fa8d98581432753e
andikanugraha11 <m.andika.nugraha@gmail.com>
2016-03-30 01:04:51 +0700

    sd

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Andika Nugraha</title>
+</head>
+<body>
 <h1>TEST HOSTING GITHUB</h1>
 <a href="http://twitter.com/andikanugraha11">Twitter</a>
+</body>
+</html>
